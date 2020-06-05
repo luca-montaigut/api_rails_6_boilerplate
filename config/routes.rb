cc69d35f90f7e8f3430c8c803c87d042a26eb64a
Luca Montaigut <lmontaigut@gmail.com>
2020-06-05 14:45:27 +0200

    first commit, remember to add .env before start

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # resources here
+    end
+  end
+end
